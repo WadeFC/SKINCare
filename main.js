3ad87d10b780dbeba8bd62310238c3140ec0f3fd
Obi-chijindu Chimdiebele <113168769+WadeFC@users.noreply.github.com>
2022-11-18 14:46:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+// no 1 
+// truth = [2,1,0,3,5,4,6,7,9,8];
+// maxy = truth[0];
+// for (var i = 0 ; i < truth.length ; i ++){
+//     if (truth[i]>maxy)
+//     {maxy = truth[i];}
+// }
+// console.log(maxy + " is the highest number");
+
+// no 2
+// truth = [2,1,0,3,5,4,6,7,9,8];
+// mini = truth[0];
+// for (var i = 0 ; i < truth.length ; i ++){
+//     if (truth[i]<mini)
+//     {mini = truth[i];}
+// }
+// console.log(mini + " is the lowest number");
+
+
+
+
+// no 3
+
+// no 3i
+//         Negative = [];
+//         Positive =[];
+//         arrT = [2,-1,-2,-4,0,1,5,3,-3,4,-1-5,-6,-7];
+//         for (var i = 0 ; i < arrT.length ; i ++)
+//         {
+//             if(arrT[i] < 0)
+//             {
+//                 Negative.push(arrT[i])
+//             }
+//             else if(arrT[i] >= 0)
+//             {
+//                 Positive.push(arrT[i])
+//             }
+            
+//         };
+
+
+    
+// //    no 3ii
+    
+//     NegaMaxy = Math.max.apply(Math,Negative);
+//     NegaMini = Math.min.apply(Math,Negative);
+    
+//     PosiMaxy = Math.max.apply(Math,Positive);
+//     PosiMini = Math.min.apply(Math,Positive);
+
+
+//     console.log(PosiMini);
+// made some changes
